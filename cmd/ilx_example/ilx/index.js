7dcea5dd193de2884eac8cf6ab744782c6fb9f65
Dimitar Rusev <drusev@efellows.bg>
2024-09-16 17:34:42 +0300

    Change JS example

--- cmd/ilx_example/ilx/index.js
+++ cmd/ilx_example/ilx/index.js
@@ -1,26 +1,10 @@
-var f5 = require("f5-nodejs");
-
+var f5 = require('f5-nodejs');
 var ilx = new f5.ILXServer();
 
-ilx.addMethod("getCredentials", function (req, res) {
-  const [user] = req.params();
-  const arc = new CyberArc();
-  const credentials = arc.getCredentials(user);
-  res.reply(credentials);
+ilx.addMethod('greeter', function(req, res) {
+  var arg = req.params()[0];
+
+  res.reply('Hello ' + arg);
 });
 
 ilx.listen();
-
-/**
- * @class CyberArc
- */
-class CyberArc {
-  /**
-   * @method getCredentials
-   * @param {string} user
-   * @returns {Array} [username, password]
-   */
-  getCredentials(username) {
-    return ["admin", "password"];
-  }
-}
